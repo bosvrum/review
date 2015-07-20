60c626f093745faeeab7b1ea040e3546b43135fe
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-20 12:50:14 +0200

    Add update action for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,19 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie succesfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
 end
